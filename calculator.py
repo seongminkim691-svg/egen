a2ce46b35c2bfbf0d37bd0feea3626b39f6fe22e
Kimseongmin <seongminkim691@gmail.com>
2025-12-01 14:27:19 +0900

    Modify calculating machine for free

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-# 기본 계산기
+# 기본 계산기 
+# 무료판에서는 더하기, 빼기, 곱하기, 나눗셈만 제공
 def add(a, b):
 	return a+b
 
@@ -11,26 +12,3 @@ def multifly(a, b):
 def divide_new(a,b):
 	return a/b
 	
-def getMedian(a, b):
-    return (a+b)/2
-
-def get_Abs(a,b):
-	if num>= 0:
-		return num
-	else:
-		return -num
-
-def get_Percent(a, b):
-	return (a/b) * 100
-
-def get_Sum_var1(n):
-	return n(n+1)/2
-
-def factorial(n):
-
-	def factorial(n):
-    num = 1
-    while n >= 1:
-        num = num * n
-        n = n - 1
-    return num
