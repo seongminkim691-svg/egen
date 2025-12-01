47a36d5366d99bedee304e59ebb29f6c226edd4b
Kimseongmin <seongminkim691@gmail.com>
2025-12-01 14:22:56 +0900

    Add more discription at README.md

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# 기본 계산기(다시 수정 var2)
+# 기본 계산기
 def add(a, b):
 	return a+b
 
@@ -8,5 +8,29 @@ def subtract(a, b):
 def multifly(a, b):
 	return a*b
 
-def divide_free(a,b):
+def divide_new(a,b):
 	return a/b
+	
+def getMedian(a, b):
+    return (a+b)/2
+
+def get_Abs(a,b):
+	if num>= 0:
+		return num
+	else:
+		return -num
+
+def get_Percent(a, b):
+	return (a/b) * 100
+
+def get_Sum_var1(n):
+	return n(n+1)/2
+
+def factorial(n):
+
+	def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
